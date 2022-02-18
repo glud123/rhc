2ac6d45f3d2e37acba3ed93a483a7168c7697a81
glud123 <glud123@163.com>
2022-02-18 20:09:45 +0800

    feat: add form

--- site/src/views/ComponentsPage/index.tsx
+++ site/src/views/ComponentsPage/index.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import Form, { useForm } from "@rhc/form";
 
 const Page = () => {
-  const form = useForm("form");
+  const form = useForm("form1");
+  
   return (
     <div>
       <Form form={form}>
-        <Form.Item name="name" label="姓名"></Form.Item>
-        <Form.Item name="age" label="年龄"></Form.Item>
-        <Form.Item name="gender" label="性别"></Form.Item>
+        <Form.Item name="name" label="姓名">
+          <input />
+        </Form.Item>
+        <Form.Item name="age" label="年龄">
+          <input />
+        </Form.Item>
+        <Form.Item name="gender" label="性别">
+          <input />
+        </Form.Item>
       </Form>
     </div>
   );
