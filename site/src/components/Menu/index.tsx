31ccdfffcd60c0812c60c9f0e2386f50a4e35853
glud123 <glud123@163.com>
2022-02-21 23:29:20 +0800

    fix: add icon

--- site/src/components/Menu/index.tsx
+++ site/src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createUseStyles } from "react-jss";
 
@@ -11,6 +11,13 @@ const Menu: FC<MenuProps> = (props) => {
   const styles = useStyles();
 
   const location = useLocation();
+
+  useEffect(() => {
+    let active = options.find((item) => item.path === location.pathname);
+    if (active && active.name) {
+      document.title = `${active.name} | React Hook Component`;
+    }
+  }, [location.pathname]);
 
   let nextOptions = options.filter((option) => {
     return !option.hidden;
--- site/src/views/ComponentsPage/index.tsx
+++ site/src/views/ComponentsPage/index.tsx
@@ -9,23 +9,70 @@ const Page = () => {
   const [formData, setFormData] = useState<any>();
 
   const handleClick = () => {
-    let value = form.get();
-    setFormData({ ...value });
+    form.validate().then((data) => {
+      console.log(data);
+      let value = form.get();
+      setFormData({ ...value });
+    });
   };
 
   return (
     <div>
       <Form form={form}>
-        <Form.Item name="name" label="姓名">
+        <Form.Item
+          name="name"
+          label="姓名"
+          required
+          rules={(value: any) => {
+            if (value) {
+              return Promise.resolve();
+            } else {
+              return Promise.reject(new Error("请输入姓名"));
+            }
+          }}
+        >
           <Input placeholder="请输入姓名" />
         </Form.Item>
-        <Form.Item name="age" label="年龄">
+        <Form.Item
+          name="age"
+          label="年龄"
+          required
+          rules={(value: any) => {
+            if (value) {
+              return Promise.resolve();
+            } else {
+              return Promise.reject(new Error("请输入年龄"));
+            }
+          }}
+        >
           <Input type="number" placeholder="请输入年龄" />
         </Form.Item>
-        <Form.Item name="address" label="地址">
+        <Form.Item
+          name="address"
+          label="地址"
+          required
+          rules={(value: any) => {
+            if (value) {
+              return Promise.resolve();
+            } else {
+              return Promise.reject(new Error("请输入地址"));
+            }
+          }}
+        >
           <Input placeholder="请输入地址" />
         </Form.Item>
-        <Form.Item name="class" label="班级">
+        <Form.Item
+          name="class"
+          label="班级"
+          required
+          rules={(value: any) => {
+            if (value) {
+              return Promise.resolve();
+            } else {
+              return Promise.reject(new Error("请输选择班级"));
+            }
+          }}
+        >
           <Select
             placeholder="请选择班级"
             options={[
